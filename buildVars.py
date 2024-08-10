8a2d7cc392d519dcbf968f4180cfc203f674381b
jason_python50 <emmanuelasamoah179@gmail.com>
2024-08-10 04:03:23 +0000

    Initial commit

---
+++ buildVars.py
@@ -0,0 +1,69 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
+# Add-on information variables
+addon_info = {
+	# add-on Name/identifier, internal for NVDA
+	"addon_name": "referenceStyler",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on
+	# to be shown on installation and add-on information found in Add-ons Manager.
+	"addon_summary": _("Reference Styler"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description": _("""This add-on allows users to manage and insert references in various citation styles.
+It supports APA, MLA, Chicago, Vancouver, Harvard, IEEE, AMA, and ACS styles."""),
+	# version
+	"addon_version": "24.0.0",
+	# Author(s)
+	"addon_author": "Asamoah Emmanuel <emmanuelasamoah179@gmail.com>",
+	# URL for the add-on documentation support
+	"addon_url": None,
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": None,
+	# Documentation file name
+	"addon_docFileName": "readme.html",
+	# Minimum NVDA version supported
+	"addon_minimumNVDAVersion": "2023.3",
+	# Last NVDA version supported/tested
+	"addon_lastTestedNVDAVersion": "2024.1",
+	# Add-on update channel (default is None, denoting stable releases)
+	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL v2",
+	# URL for the license document the add-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
+}
+
+# Define the python files that are the sources of your add-on.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+pythonSources = ["addon/globalPlugins/referenceStyler/*.py"]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
+
+# Base language for the NVDA add-on
+baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+markdownExtensions = []
+
+# Custom braille translation tables
+brailleTables = {}
---
+++ readme.md
@@ -0,0 +1,68 @@
+# Reference Styler
+
+## Introduction
+
+Reference Styler is an NVDA add-on that allows users to manage and insert references in various citation styles. It supports APA, MLA, Chicago, Vancouver, Harvard, IEEE, AMA, and ACS styles.
+
+## Features
+
+- Manage a list of references
+- Insert references in different citation styles
+- Support for multiple citation styles: APA, MLA, Chicago, Vancouver, Harvard, IEEE, AMA, and ACS
+- Easy-to-use interface for adding, editing, and removing references
+- Keyboard shortcuts for quick insertion of references
+
+
+## Usage
+
+### Managing References
+
+To open the Reference Manager:
+
+1. Press NVDA+Shift+F
+2. Use the "Add" button to add new references
+3. Use the "Remove" button to delete selected references
+4. Use the "Save" button to save your reference list
+5. Use the "Load" button to load a previously saved reference list
+
+### Selecting a Reference
+
+To select a reference for insertion:
+
+1. Press NVDA+Shift+S
+2. Choose a reference from the list that appears
+3. Press Enter to confirm your selection
+
+### Inserting References
+
+To insert a reference, use the following keystrokes:
+
+- NVDA+Shift+A: Insert APA style reference
+- NVDA+Shift+M: Insert MLA style reference
+- NVDA+Shift+C: Insert Chicago style reference
+- NVDA+Shift+V: Insert Vancouver style reference
+- NVDA+Shift+H: Insert Harvard style reference
+- NVDA+Shift+I: Insert IEEE style reference
+- NVDA+Shift+D: Insert AMA style reference
+- NVDA+Shift+E: Insert ACS style reference
+
+## Notes
+
+- If you're not in an editable text field when you try to insert a reference, the formatted reference will be copied to your clipboard instead.
+- Make sure to select a reference using NVDA+Shift+S before attempting to insert it.
+
+## Support
+
+For support, feature requests, or bug reports, please contact the add-on author or visit the add-on's GitHub repository.
+
+## License
+
+This add-on is licensed under the GNU General Public License v2.
+
+## Author
+
+Asamoah Emmanuel
+
+---
+
+I hope you find Reference Styler useful in your academic and professional writing endeavors!
